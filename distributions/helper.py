8e5e3794c1f705022b8af53e7d62478aed4c7bbe
aloctavodia <aloctavodia@gmail.com>
2025-09-23 10:11:06 +0300

    remove unused functions

--- distributions/helper.py
+++ distributions/helper.py
@@ -1,29 +1,4 @@
-import numpy as np
 import pytensor.tensor as pt
-
-eps = np.finfo(float).eps
-
-def from_precision(precision):
-    sigma = 1 / precision**0.5
-    return sigma
-
-def to_precision(sigma):
-    precision = 1 / (eps + sigma**2)
-    return precision
-
-
-def all_not_none(*args):
-    for arg in args:
-        if arg is None:
-            return False
-    return True
-
-
-def any_not_none(*args):
-    for arg in args:
-        if arg is not None:
-            return True
-    return False
 
 
 def ppf_bounds_cont(value, q, lower, upper):
@@ -31,5 +6,5 @@ def ppf_bounds_cont(value, q, lower, upper):
     return pt.switch(
         pt.and_(q >= lower, q <= upper),
         value,
-        np.nan,
+        pt.nan,
     )
