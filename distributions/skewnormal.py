e1cfe8016ef227664b85d46338dda6f2242cd2a1
aloctavodia <aloctavodia@gmail.com>
2025-10-02 16:33:29 +0300

    add distributions from student t family

---
+++ distributions/skewnormal.py
@@ -0,0 +1,101 @@
+import pytensor.tensor as pt
+
+from .halfnormal import entropy as halfnormal_entropy
+from .normal import entropy as normal_entropy
+
+
+def mean(mu, sigma, alpha):
+    mu_b, sigma_b, alpha_b = pt.broadcast_arrays(mu, sigma, alpha)
+    return mu_b + sigma_b * pt.sqrt(2 / pt.pi) * alpha_b / pt.sqrt(1 + alpha_b**2)
+
+
+def mode(mu, sigma, alpha):
+    # For skew normal, the mode is approximately mu + sigma * delta * sqrt(2/pi)
+    # where delta = alpha / sqrt(1 + alpha^2), but this is an approximation
+    mu_b, sigma_b, alpha_b = pt.broadcast_arrays(mu, sigma, alpha)
+    delta = alpha_b / pt.sqrt(1 + alpha_b**2)
+    return mu_b + sigma_b * delta * pt.sqrt(2 / pt.pi)
+
+
+def median(mu, sigma, alpha):
+    return ppf(0.5, mu, sigma, alpha)
+
+
+def var(mu, sigma, alpha):
+    _, sigma_b, alpha_b = pt.broadcast_arrays(mu, sigma, alpha)
+    delta = alpha_b / pt.sqrt(1 + alpha_b**2)
+    return sigma_b**2 * (1 - 2 * delta**2 / pt.pi)
+
+
+def std(mu, sigma, alpha):
+    return pt.sqrt(var(mu, sigma, alpha))
+
+
+def skewness(mu, sigma, alpha):
+    delta = alpha / pt.sqrt(1 + alpha**2)
+    mean_z = pt.sqrt(2 / pt.pi) * delta
+    return ((4 - pt.pi) / 2) * (mean_z**3 / (1 - mean_z**2)**(3/2))
+
+
+def kurtosis(mu, sigma, alpha):
+    _, _, alpha_b = pt.broadcast_arrays(mu, sigma, alpha)
+    delta = alpha_b / pt.sqrt(1 + alpha_b**2)
+    mean_z = delta * pt.sqrt(2 / pt.pi)
+    var_z = 1 - 2 * delta**2 / pt.pi
+    return 2 * (pt.pi - 3) * (mean_z**4 / var_z**2)
+
+
+def entropy(mu, sigma, alpha):
+    # The entropy of the skew normal distribution does not have a simple closed form
+    # we use a linear combination of the normal and half-normal entropies as approximation
+    H_normal = normal_entropy(mu, sigma)
+    H_halfn = halfnormal_entropy(sigma)
+    
+    factor = 1 / pt.sqrt(1 + alpha**2)
+    return H_normal * factor +  H_halfn * (1 - factor)
+
+
+
+def cdf(x, mu, sigma, alpha):
+    return 0.5 * (1 + pt.erf((x - mu) / (sigma * 2**0.5))) - 2 * pt.owens_t((x - mu) / sigma, alpha)
+
+
+def logcdf(x, mu, sigma, alpha):
+    return pt.log(cdf(x, mu, sigma, alpha))
+
+def isf(x, mu, sigma, alpha):
+    #return ppf(1 - x, mu, sigma, alpha)
+    raise NotImplementedError("ISF for skewnormal is not implemented yet.")
+
+
+def pdf(x, mu, sigma, alpha):
+    return pt.exp(logpdf(x, mu, sigma, alpha))
+
+
+def ppf(q, mu, sigma, alpha):
+    # In PreliZ we use a root-finding method to compute the PPF
+    raise NotImplementedError("PPF for skewnormal is not implemented yet.")
+
+def sf(x, mu, sigma, alpha):
+    return 1 - cdf(x, mu, sigma, alpha)
+
+
+def rvs(mu, sigma, alpha, size=None, random_state=None):
+    mu_b, sigma_b, alpha_b = pt.broadcast_arrays(mu, sigma, alpha)    
+    u_0 = pt.random.normal(0, 1, rng=random_state, size=size)
+    v = pt.random.normal(0, 1, rng=random_state, size=size)
+    d = alpha_b / pt.sqrt(1 + alpha_b**2)
+    u_1 = d * u_0 + v * pt.sqrt(1 - d**2)
+    return pt.sign(u_0) * u_1 * sigma_b + mu_b
+
+
+def logpdf(x, mu, sigma, alpha):
+    tau = 1.0 / (sigma**2)
+    return (
+            pt.log(1 + pt.erf(((x - mu) * pt.sqrt(tau) * alpha) / pt.sqrt(2)))
+            + (-tau * (x - mu) ** 2 + pt.log(tau / pt.pi / 2.0)) / 2.0
+        )
+
+
+def logsf(x, mu, sigma, alpha):
+    return pt.log1mexp(logcdf(x, mu, sigma, alpha))
--- tests/test_scipy.py
+++ tests/test_scipy.py
@@ -6,84 +6,214 @@ from scipy import stats
 
 from distributions import beta as Beta
 from distributions import betascaled as BetaScaled
+from distributions import cauchy as Cauchy
+from distributions import halfcauchy as HalfCauchy
+from distributions import halfnormal as HalfNormal
+from distributions import halfstudentt as HalfStudentT
 from distributions import normal as Normal
-
+from distributions import skewnormal as SkewNormal
+from distributions import studentt as StudentT
 
 
 @pytest.mark.parametrize(
-    "p_dist, sp_dist, p_params, sp_params, support",
+    "name, p_dist, sp_dist, p_params, sp_params, support",
     [
-    (Beta, stats.beta, (pt.constant(2.), pt.constant(5.)), (2, 5), (0, 1)),
-    (BetaScaled, stats.beta, (pt.constant(2.), pt.constant(5.), pt.constant(-1.), pt.constant(3.)), (2, 5, -1, 4), (-1, 3)),
-    (Normal, stats.norm, (pt.constant(0.), pt.constant(2.)), (0, 2), (-np.inf, np.inf)),
+        ("_", Beta, stats.beta, (pt.constant(2.0), pt.constant(5.0)), {"a": 2, "b": 5}, (0, 1)),
+        (
+            "_",
+            BetaScaled,
+            stats.beta,
+            (pt.constant(2.0), pt.constant(5.0), pt.constant(-1.0), pt.constant(3.0)),
+            {"a": 2, "b": 5, "loc": -1, "scale": 4},
+            (-1, 3),
+        ),
+        (
+            "_",
+            Cauchy,
+            stats.cauchy,
+            (pt.constant(2.0), pt.constant(4.5)),
+            {"loc": 2.0, "scale": 4.5},
+            (-np.inf, np.inf),
+        ),
+        (
+            "halfstudent0",
+            HalfStudentT,
+            stats.halfnorm,
+            (pt.constant(1e4), pt.constant(2.0)),
+            {"loc": 0, "scale": 2},
+            (0, np.inf),
+        ),
+        (
+            "halfstudent1",
+            HalfStudentT,
+            stats.halfcauchy,
+            (pt.constant(1.0), pt.constant(3.5)),
+            {"scale": 3.5},
+            (0, np.inf),
+        ),
+        (
+            "_",
+            Normal,
+            stats.norm,
+            (pt.constant(10), pt.constant(2)),
+            {"loc": 10, "scale": 2},
+            (-np.inf, np.inf),
+        ),
+        (
+            "_",
+            HalfCauchy,
+            stats.halfcauchy,
+            (pt.constant(3.5),),
+            {"scale": 3.5},
+            (0, np.inf),
+        ),
+        (
+            "_",
+            HalfNormal,
+            stats.halfnorm,
+            (pt.constant(2),),
+            {"loc": 0, "scale": 2},
+            (-np.inf, np.inf),
+        ),
+        (
+            "skewnormal",
+            SkewNormal,
+            stats.skewnorm,
+            (pt.constant(0.5), pt.constant(2), pt.constant(-2)),
+            {"a": -2, "loc": 0.5, "scale": 2},
+            (-np.inf, np.inf),
+        ),
+        (
+            "_",
+            StudentT,
+            stats.t,
+            (pt.constant(5), pt.constant(0), pt.constant(2)),
+            {"df": 5, "loc": 0, "scale": 2},
+            (-np.inf, np.inf),
+        ),
     ],
 )
-def test_match_scipy(p_dist, sp_dist, p_params, sp_params, support):
-    scipy_dist = sp_dist(*sp_params)
+def test_match_scipy(name, p_dist, sp_dist, p_params, sp_params, support):
+    scipy_dist = sp_dist(**sp_params)
 
     # Entropy
     actual = p_dist.entropy(*p_params).eval()
     expected = scipy_dist.entropy()
-    assert_almost_equal(actual, expected, decimal=4)
+    if name in ["skewnormal", "halfstudent0"]:
+        assert_almost_equal(actual, expected, decimal=2)
+    else:
+        assert_almost_equal(actual, expected, decimal=4)
 
     # Random variates
-    rng = pt.random.default_rng(1)
-    actual_rvs = p_dist.rvs(*p_params, size=20, random_state=rng).eval()
-    rng = np.random.default_rng(1)
-    expected_rvs = scipy_dist.rvs(20, random_state=rng)
-    assert_almost_equal(actual_rvs, expected_rvs)
+    rng_p = pt.random.default_rng(1)
+    actual_rvs = p_dist.rvs(*p_params, size=20, random_state=rng_p).eval()
+    rng_n = np.random.default_rng(1)
+    expected_rvs = scipy_dist.rvs(20, random_state=rng_n)
+
+    if name in ["halfstudent0", "halfstudent1"]:
+        p_rvs = p_dist.rvs(*p_params, size=20_000, random_state=rng_p).eval()
+        s_rvs = scipy_dist.rvs(20_000, random_state=rng_n)
+        assert_almost_equal(
+            np.quantile(p_rvs, [0.25, 0.5, 0.75]), np.quantile(s_rvs, [0.25, 0.5, 0.75]), decimal=1
+        )
+    elif name in ["skewnormal"]:
+        p_rvs = p_dist.rvs(*p_params, size=20_000).eval()
+        s_rvs = scipy_dist.rvs(20_000)
+        assert_almost_equal(
+            np.quantile(p_rvs, [0.25, 0.5, 0.75]), np.quantile(s_rvs, [0.25, 0.5, 0.75]), decimal=1
+        )
+    else:
+        assert_almost_equal(actual_rvs, expected_rvs)
 
     extended_vals = np.concatenate(
-            [
-                actual_rvs,
-                support,
-                [support[0] - 1],
-                [support[0] - 2],
-                [support[1] + 1],
-                [support[1] + 2],
-            ]
-        )
+        [
+            actual_rvs,
+            support,
+            [support[0] - 1],
+            [support[0] - 2],
+            [support[1] + 1],
+            [support[1] + 2],
+        ]
+    )
 
     # PDF
     actual_pdf = p_dist.pdf(extended_vals, *p_params).eval()
     expected_pdf = scipy_dist.pdf(extended_vals)
-    assert_almost_equal(actual_pdf, expected_pdf, decimal=4)
+
+    if name in ["halfstudent0"]:
+        assert_almost_equal(actual_pdf, expected_pdf, decimal=2)
+    else:
+        assert_almost_equal(actual_pdf, expected_pdf, decimal=4)
 
     # logPDF
     actual_logpdf = p_dist.logpdf(extended_vals, *p_params).eval()
     expected_logpdf = scipy_dist.logpdf(extended_vals)
-    assert_almost_equal(actual_logpdf, expected_logpdf)
+
+    if name in ["halfstudent0"]:
+        assert_almost_equal(actual_logpdf, expected_logpdf, decimal=2)
+    else:
+        assert_almost_equal(actual_logpdf, expected_logpdf)
 
     # CDF
     actual_cdf = p_dist.cdf(extended_vals, *p_params).eval()
     expected_cdf = scipy_dist.cdf(extended_vals)
-    assert_almost_equal(actual_cdf, expected_cdf, decimal=6)
+
+    if name in ["halfstudent0"]:
+        assert_almost_equal(actual_cdf, expected_cdf, decimal=2)
+    else:
+        assert_almost_equal(actual_cdf, expected_cdf, decimal=6)
 
     # logCDF
     actual_logcdf = p_dist.logcdf(extended_vals, *p_params).eval()
     expected_logcdf = scipy_dist.logcdf(extended_vals)
-    assert_almost_equal(actual_logcdf, expected_logcdf, decimal=5)
+
+    if name in ["halfstudent0"]:
+        assert_almost_equal(actual_logcdf, expected_logcdf, decimal=2)
+    else:
+        assert_almost_equal(actual_logcdf, expected_logcdf, decimal=5)
 
     # SF
     actual_sf = p_dist.sf(extended_vals, *p_params).eval()
     expected_sf = scipy_dist.sf(extended_vals)
-    assert_almost_equal(actual_sf, expected_sf, decimal=6)
+
+    if name in ["halfstudent0"]:
+        assert_almost_equal(actual_sf, expected_sf, decimal=2)
+    else:
+        assert_almost_equal(actual_sf, expected_sf, decimal=6)
 
     # logSF
     actual_logsf = p_dist.logsf(extended_vals, *p_params).eval()
     expected_logsf = scipy_dist.logsf(extended_vals)
-    assert_almost_equal(actual_logsf, expected_logsf, decimal=4)
+    if name in ["halfstudent0"]:
+        assert_almost_equal(actual_logsf, expected_logsf, decimal=2)
+    else:
+        assert_almost_equal(actual_logsf, expected_logsf, decimal=4)
 
     # PPF
     x_vals = np.array([-1, 0, 0.25, 0.5, 0.75, 1, 2])
-    actual_ppf = p_dist.ppf(x_vals, *p_params).eval()
-    expected_ppf = scipy_dist.ppf(x_vals)
-    assert_almost_equal(actual_ppf, expected_ppf)
+    if name in ["skewnormal"]:
+        # ppf method is not implemented yet
+        pass
+    else:
+        actual_ppf = p_dist.ppf(x_vals, *p_params).eval()
+        expected_ppf = scipy_dist.ppf(x_vals)
+
+        if name in ["halfstudent0"]:
+            assert_almost_equal(actual_ppf, expected_ppf, decimal=2)
+        else:
+            assert_almost_equal(actual_ppf, expected_ppf)
 
     # ISF
-    actual_isf = p_dist.isf(x_vals, *p_params).eval()
-    expected_isf = scipy_dist.isf(x_vals)
-    assert_almost_equal(actual_isf, expected_isf)
+    if name in ["skewnormal"]:
+        # isf method is not implemented yet
+        pass
+    else:
+        actual_isf = p_dist.isf(x_vals, *p_params).eval()
+        expected_isf = scipy_dist.isf(x_vals)
+        if name in ["halfstudent0"]:
+            assert_almost_equal(actual_isf, expected_isf, decimal=2)
+        else:
+            assert_almost_equal(actual_isf, expected_isf)
 
     # mean
     mean = p_dist.mean(*p_params).eval()
@@ -91,9 +221,17 @@ def test_match_scipy(p_dist, sp_dist, p_params, sp_params, support):
     assert_almost_equal(mean, expected_mean)
 
     # median
-    median = p_dist.median(*p_params).eval()
-    expected_median = scipy_dist.median()
-    assert_almost_equal(median, expected_median)
+    if name in ["skewnormal"]:
+        # ppf method is not implemented yet
+        pass
+    else:
+        median = p_dist.median(*p_params).eval()
+        expected_median = scipy_dist.median()
+
+        if name in ["halfstudent0", "halfstudent1"]:
+            assert_almost_equal(median, expected_median, decimal=2)
+        else:
+            assert_almost_equal(median, expected_median)
 
     # mode
     finite_expected_pdf = np.where(np.isfinite(expected_pdf), expected_pdf, -np.inf)
@@ -101,7 +239,7 @@ def test_match_scipy(p_dist, sp_dist, p_params, sp_params, support):
     actual_mode = p_dist.mode(*p_params).eval()
     assert_almost_equal(actual_mode, expected_mode, decimal=0)
 
-    # standard deviation   
+    # standard deviation
     std = p_dist.std(*p_params).eval()
     expected_std = scipy_dist.std()
     assert_almost_equal(std, expected_std)
@@ -114,9 +252,11 @@ def test_match_scipy(p_dist, sp_dist, p_params, sp_params, support):
     # skewness
     skewness = p_dist.skewness(*p_params).eval()
     expected_skewness = scipy_dist.stats(moments="s")
-    assert_almost_equal(skewness, expected_skewness)
+    if name not in ["halfstudent0"]:
+        assert_almost_equal(skewness, expected_skewness)
 
     # kurtosis
     kurtosis = p_dist.kurtosis(*p_params).eval()
     expected_kurtosis = scipy_dist.stats(moments="k")
-    assert_almost_equal(kurtosis, expected_kurtosis)
+    if name not in ["halfstudent0"]:
+        assert_almost_equal(kurtosis, expected_kurtosis)
