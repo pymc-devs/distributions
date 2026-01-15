c53eeb66ef1d1db7850024b37f96262632a0516c
Chris Fonnesbeck <fonnesbeck@gmail.com>
2026-01-14 21:33:09 -0600

    Fixes in response to review comments

--- tests/test_loglogistic.py
+++ tests/test_loglogistic.py
@@ -26,31 +26,4 @@ def test_loglogistic_vs_scipy(params, sp_params):
         sp_params=sp_params,
         support=support,
         name="loglogistic",
-        skip_skewness=sp_params["c"] <= 3,
-        skip_kurtosis=sp_params["c"] <= 4,
-    )
-
-
-@pytest.mark.parametrize(
-    "params, sp_params",
-    [
-        ([1.0, 1.5], {"c": 1.5, "scale": 1.0}),
-        ([2.0, 1.2], {"c": 1.2, "scale": 2.0}),
-    ],
-)
-def test_loglogistic_low_beta(params, sp_params):
-    p_params = make_params(*params, dtype="float64")
-    support = (0, float("inf"))
-
-    run_distribution_tests(
-        p_dist=LogLogistic,
-        sp_dist=stats.fisk,
-        p_params=p_params,
-        sp_params=sp_params,
-        support=support,
-        name="loglogistic_low_beta",
-        skip_variance=True,
-        skip_standard_deviation=True,
-        skip_skewness=True,
-        skip_kurtosis=True,
     )
